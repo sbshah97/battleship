0d4e8e04828d7f3f08e1d6051d422862bcd04380
jskimko <jskimko@email.wm.edu>
2016-10-13 19:55:33 -0400

    Added simple board graphics.

--- src/battleship.py
+++ src/battleship.py
@@ -2,15 +2,17 @@ import random
 # Welcome to the game of Battleship
 # @Author: Salman Shah
 
+def print_ocean(ocean):
+    for r in range(5):
+        print "%s|%s|%s|%s|%s " % (ocean[r][0],ocean[r][1],ocean[r][2],ocean[r][3],ocean[r][4])
+        if r < 4:
+            print "-+-+-+-+-"
+
 def main():
     # Initializing the 'Ocean'
-    ocean = [["" for x in range(0,5)] for y in range(0,5)]  # declare a 2d matrix ocean
+    ocean = [[" "]*5 for _ in range(5)]
 
     # --- Game Starts here --- #
-    def print_ocean(ocean):
-    	for row in ocean:
-    		print " ".join(row)
-
     # Starting the game and printing the 'Ocean'
 
     print "Let the game of 5 x 5 Battleship begin"
@@ -18,8 +20,8 @@ def main():
     print_ocean(ocean)
 
     # Ask user to place the ship
-    place_row = int(raw_input("Enter a valid row to place the Ship!"))
-    place_column = int(raw_input("Enter a valid column to place the Ship!"))
+    place_row = int(raw_input("Enter a valid row to place the Ship: "))
+    place_column = int(raw_input("Enter a valid column to place the Ship: "))
     ocean[place_row][place_column] = "O" # To show where the ship is placed 
     board = 5 #initialize the size of the board 
     for turn in range(4):
@@ -37,11 +39,11 @@ def main():
     		ocean[guess_row][guess_column] = 'X'
     		print "Sorry man you missed it."
 
-    	print "This was your" + str(turn+1) + "turn"
+    	print "This was turn " + str(turn+1) + "."
     	print_ocean(ocean)
 
     if turn >= 3:
-    	print "Game over"
+    	print "Game over."
 	
     # --- Game ends here --- #
 
